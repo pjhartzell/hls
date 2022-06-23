ec52f1a1ef540bd168b79d4e98f0037a61bd4197
pjhartzell <preston.hartzell@gmail.com>
2022-06-23 16:59:36 -0400

    initial setup and renaming

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,5 +1,5 @@
-import stactools.ephemeral
+import stactools.hls
 
 
 def test_version() -> None:
-    assert stactools.ephemeral.__version__ is not None
+    assert stactools.hls.__version__ is not None
